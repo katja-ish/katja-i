a86e1e14b3fa56feab418f344e8a70ee94f7c808
Katja <katyanikitina.me@yandex.ru>
2025-07-06 17:13:07 +0100

    new desc block experiment

--- src/Pages/Works/index.js
+++ src/Pages/Works/index.js
@@ -42,8 +42,17 @@ const Works = (props) => {
             <Paragraph lineOne={"Interested in adding to this world. "} lineTwo={"Less machinery, more beauty"} />
 
             <main className="Works">{worksJSX}</main>
+
+             <section>
+                <h1>as a designer, i love being at the very beginning — when something is just a thought. i enjoy gently unfolding ideas, scenarios, visuals. i can also fit easily in a bigger team, be patient and attentive to a client.<br/>
+                <br/>i <i>care</i> about design systems — building them from scratch or supporting.<br/>
+                <br/>i <i>stand</i> between design and code — translating meaning and intention.<br/>
+                <br/>i <i>manage</i> tasks with care — keeping things clear and organized. <br/> <br/> your project might be mobile or web, conceptual or practical. i'm drawn to ones with purpose: social, art, nature-based, people-centered. if it makes me feel something — i’m curious.<br/>
+                <br/>let’s discuss & work.</h1> <br/>
+             </section>
         </>
       );
+      
       
   };
 
